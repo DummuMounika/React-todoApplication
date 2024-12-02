0b11ed65686ab77161fca2c94714f2f4999d4312
DummuMounika <mounikadummu.edu2024@gmail.com>
2024-12-01 19:38:30 -0600

    Added localstorage implementation

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,16 +1,47 @@
-export default  function TodoItem({task, toggleCompletion, deleteToDo, editToDo}) {
+import { useState } from "react";
+
+export default  function TodoItem({task, toggleCompletion, deleteToDo, editToDo, addToDoDate}) {
+  const [selectDate,setSelectedDate] = useState(task.toBeDone||'');
+  console.log(" seleted date:",selectDate)
+  const handleDateChange = (event) => {
+      const newDate=event.target.value;
+      setSelectedDate(newDate);
+      addToDoDate(task.id,newDate);
+    };
+
+  // const toggleCompletion = () => {
+  //   setSelectedDate(selectDate);
+  // }
+
   return (
     <>
       <div className="todo-item">
         <p>
-          {task.task} - {task.completed ? "Completed" : "Pending"}
+            {task.task} {task.completed ? "Completed" : "Pending"}
         </p>
         <div className="button-container">
-          <button className="btn-edit" onClick={()=>editToDo(task.id)}>Edit Task</button>
-          <button className="btn-delete" onClick={()=>deleteToDo(task.id)}>Delete Task</button>
+          <button className="btn-edit" onClick={()=>editToDo(task.id)}>
+            Edit Task
+          </button>
+          <button className="btn-delete" onClick={()=>deleteToDo(task.id)}>
+            Delete Task
+          </button>
+          <input 
+            type="date"  
+            value={selectDate} 
+            onChange={handleDateChange} 
+          />
+          <p>
+             {task.toBeDone !== "" ? `To be Done on: ${selectDate}` : "No due date set"}
+          </p>
+            
+          
           <button className="btn-Taskstatus"
-           onClick={() => {toggleCompletion(task.id)}}>
-            Mark as Completed</button>
+           onClick={() => {
+            toggleCompletion(task.id);
+            }}>
+            {task.completed ?  "Mark as Pending": "Mark as Completed"}
+            </button>
         </div>
       </div>
     </>
--- src/components/TodoWrapper.jsx
+++ src/components/TodoWrapper.jsx
@@ -1,15 +1,18 @@
-import React, { useReducer } from 'react'
-// import { useState } from "react";
+import React, { useEffect, useReducer, useState } from 'react'
 import TodoForm from './TodoForm'
 import TodoItem from './TodoItem';
 import EditToDoForm from './EditToDoForm';
+import { BrowserRouter as Router,Switch , Route, Routes } from 'react-router-dom';
 
+const getInitialState = () => {
+  const storedTasks = localStorage.getItem('tasks');
+  return storedTasks ? JSON.parse(storedTasks) : [];
+};
 
-const initialState = [];
 const reducer = (state, action) => {
   switch(action.type) {
     case "ADD":
-      return [...state, { id: Date.now(), task: action.payload, completed: false , isEditing: false }];
+      return [...state, { id: Date.now(), task: action.payload, completed: false , isEditing: false , tobeDone: Date()}];
     case "DELETE":
       return state.filter((item) => item.id !== action.payload);
     case "TOGGLE":
@@ -18,32 +21,76 @@ const reducer = (state, action) => {
       return state.map((item) => item.id === action.payload ? {...item, isEditing : !item.isEditing}: item);
     case "EDITTASK":
       return state.map((item) => item.id === action.payload.id ? {...item, task: action.payload.task, isEditing : !item.isEditing}: item);
+    case "ADDDATE":
+      return state.map((item) => 
+        item.id === action.payload.id ? {...item, tobeDone: action.payload.date }: item);
     default:
       return state;
     }
 
 }
 export default function TodoWrapper() {
-    const [tasks, dispatch] = useReducer(reducer, initialState);
+
+    
+    const [tasks, dispatch] = useReducer(reducer, [], getInitialState);
+
+     useEffect(() => {
+      localStorage.setItem('tasks', JSON.stringify(tasks))
+     }, [tasks]);
+
+  
 
     const addToDoTask = (task) => dispatch({type: "ADD" , payload: task});
     const editToDoTask = (id) => dispatch({type: "EDIT" , payload: id});
     const editTask = (task, id) => dispatch({type: "EDITTASK" , payload: {task, id}});
     const toggleToDoTask = (id) => dispatch({type: "TOGGLE" , payload: id});
     const deleteToDoTask = (id) => dispatch({type: "DELETE" , payload: id});
+    const addToDoDate = (id,date) => dispatch({type: "ADDDATE", payload: {id,date}});
 
+    const completedTasks = tasks.filter((task)=>task.completed);
+    const pendingTasks = tasks.filter((task)=>!task.completed);
+    
     return (
       <div>
-        <TodoForm addToDo={addToDoTask} />
-        {tasks.map((todo) => (
-            todo.isEditing ? 
-            (<EditToDoForm key={todo.id} editToDo={editTask} task={todo}/>) : 
-             (<TodoItem task={todo} 
-                      key={todo.id} 
-                      toggleCompletion={toggleToDoTask}
-                      deleteToDo = {deleteToDoTask} 
-                      editToDo= {editToDoTask}/> )
-        ))}
+
+        {/* <Router>
+              <Routes>
+              <Route path="/toDoItems" exact Component={TodoItem}></Route>
+              </Routes> */}
+              
+            <TodoForm addToDo={addToDoTask} />
+            <button>Add To Do Task</button>
+            {pendingTasks.length > 0 && <h3>Pending Task</h3>}    
+            {pendingTasks.map((todo) => (
+                todo.isEditing ?  (<EditToDoForm 
+                  key={todo.id} 
+                  editToDo={editTask}
+                  task={todo}/>) :  (<TodoItem 
+                          task={todo} 
+                          key={todo.id} 
+                          toggleCompletion={toggleToDoTask}
+                          deleteToDo = {deleteToDoTask} 
+                          editToDo= {editToDoTask}
+                          addToDoDate={addToDoDate}
+                          />)
+            ))}
+            
+            {completedTasks.length > 0 && <h3>Completed Task</h3>}
+            {completedTasks.map((todo) => (
+                todo.isEditing ?  (<EditToDoForm 
+                  key={todo.id} 
+                  editToDo={editTask}
+
+                  task={todo}/>) :  (<TodoItem 
+                          task={todo} 
+                          key={todo.id} 
+                          toggleCompletion={toggleToDoTask}
+                          deleteToDo = {deleteToDoTask} 
+                          editToDo= {editToDoTask}
+                          addToDoDate={addToDoDate}
+                          />)
+            ))}
+        {/* </Router>      */}
       </div>
     )
 }
